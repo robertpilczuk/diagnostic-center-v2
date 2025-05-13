6b745950afe393a6c7b52e4707a9653352b2e090
robertpilczuk <robertpilczuk@gmail.com>
2025-05-13 14:56:36 +0200

    feat: add Swagger UI endpoint for API documentation

--- backend/diagnostic_center/urls.py
+++ backend/diagnostic_center/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(title="Diagnostic Center API", default_version="v1"),
+    public=True,
+)
+
+urlpatterns += [
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
